0cd2185bc7029e420f3f371ad6539aa475416206
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-11-09 19:41:49 +0700

    Create and define what to do in service

--- tlcn3-app/src/app/page-get-info/page-get-info.component.ts
+++ tlcn3-app/src/app/page-get-info/page-get-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Resume } from '../models';
+import { ResumeService } from '../resume.service';
 
 @Component({
   selector: 'app-page-get-info',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-get-info.component.scss']
 })
 export class PageGetInfoComponent implements OnInit {
-
-  constructor() { }
+  resume: Resume;
+  constructor(private resumeService: ResumeService) { }
 
   ngOnInit() {
+    //retrive resume();
   }
 
 }
---
+++ tlcn3-app/src/app/resume.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResumeService {
+  //What is EventEmitter -> to let component know what change
+
+  constructor() { }
+  // Edit mode if path is /edit --> a lot of things will depend on the path
+  //Saves resume in browser
+  //Retrive last saved resume stored in browser
+  //Adds a experience to the saved resume and saves it.
+  //Update experience.
+  //Removes a experience.
+
+
+  //Adds an education to the saved resume and saves it.
+  //Update educations.
+  //Removes an education.
+
+  //Adds an skill to the saved resume and saves it.
+  //Update skill.
+  //Removes an skill.
+
+  //Adds an project to the saved resume and saves it.
+  //Update project.
+  //Removes an project.
+}
