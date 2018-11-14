c31f1e31265a6b1245c4a2adc26effd661047128
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-11-14 22:58:28 +0700

    Continue to 2way binding from main to get info

--- tlcn3-app/src/app/page-get-info/page-get-info.component.ts
+++ tlcn3-app/src/app/page-get-info/page-get-info.component.ts
@@ -1,43 +1,54 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material';
+import {
+  Component,
+  OnInit,
+  ViewContainerRef,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { MatDialog, MatDialogRef, MatDialogConfig } from "@angular/material";
 
-import { Resume } from '../models';
-import { ResumeService } from '../resume.service';
-import { Experience } from '../models';
+import { Resume } from "../models";
+import { ResumeService } from "../resume.service";
+import { Experience } from "../models";
 
 @Component({
-  selector: 'app-page-get-info',
-  templateUrl: './page-get-info.component.html',
-  styleUrls: ['./page-get-info.component.scss']
+  selector: "app-page-get-info",
+  templateUrl: "./page-get-info.component.html",
+  styleUrls: ["./page-get-info.component.scss"]
 })
 export class PageGetInfoComponent implements OnInit {
-  resume: Resume;
+  @Output() event: EventEmitter<Resume> = new EventEmitter<Resume>();
+  @Input() resume: Resume;
 
-  constructor(private dialog: MatDialog, private viewContainerRef: ViewContainerRef, public resumeService: ResumeService) { }
+  constructor(
+    private dialog: MatDialog,
+    private viewContainerRef: ViewContainerRef,
+    public resumeService: ResumeService
+  ) {}
   ngOnInit() {
-    //retrive resume();
+    console.log(this.resume);
   }
-  addNewExperience(){
+  send(): void {
+    this.event.emit();
+  }
+  addNewExperience() {
     const config = new MatDialogConfig();
-    config.width = '75vw';
-
+    config.width = "75vw";
     const dialogRef = this.dialog.open(ExperienceDialog, config);
-    
   }
 }
 
-
 //Add new Experience dialog
-@Component({
-
-}) 
-export class ExperienceDialog implements OnInit{
+@Component({})
+export class ExperienceDialog implements OnInit {
   public experience: Experience;
   public years: Array<number>;
   public months: Array<string>;
   public editMode: boolean;
-  constructor(public dialogRef: MatDialogRef<ExperienceDialog>, private resumeService: ResumeService) {
-    
-  }
-  ngOnInit(){};
+  constructor(
+    public dialogRef: MatDialogRef<ExperienceDialog>,
+    private resumeService: ResumeService
+  ) {}
+  ngOnInit() {}
 }
