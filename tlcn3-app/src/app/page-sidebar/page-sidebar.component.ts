c73148c63d94093c6b61a54f961c4f7c23959892
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-11-20 13:52:55 +0700

    Init experience success

--- tlcn3-app/src/app/page-sidebar/page-sidebar.component.ts
+++ tlcn3-app/src/app/page-sidebar/page-sidebar.component.ts
@@ -7,7 +7,7 @@ import { Location } from "@angular/common";
   styleUrls: ["./page-sidebar.component.scss"]
 })
 export class PageSidebarComponent implements OnInit {
-  userID: string = "5bf102ad63ed1c1764973631";
+  userID: string = "5bf3ac143c041324e02a5dbe";
   constructor() {}
 
   ngOnInit() {}
