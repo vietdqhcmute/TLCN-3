1fed1a2cb1e2957faf82715953839fa076292167
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-12-06 00:27:53 +0700

    Update profile UI with hidden-scroll

--- tlcn3-app/src/app/app-profile-info/app-profile-info.component.ts
+++ tlcn3-app/src/app/app-profile-info/app-profile-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../models';
 
 @Component({
   selector: 'app-app-profile-info',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-profile-info.component.scss']
 })
 export class AppProfileInfoComponent implements OnInit {
-
+  @Input() user$: User;
   constructor() { }
 
   ngOnInit() {
