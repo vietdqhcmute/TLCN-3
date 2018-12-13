3c168bf7246a7202c54ebc72e8e103e13f53a758
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-12-13 23:37:58 +0700

    Upload ok but not so fit

--- tlcn3-app/src/app/app-profile-info/app-profile-info.component.ts
+++ tlcn3-app/src/app/app-profile-info/app-profile-info.component.ts
@@ -9,23 +9,10 @@ import { UserService } from '../services/user.service';
 })
 export class AppProfileInfoComponent implements OnInit {
   @Input() user$: User;
-  imagePreview: string;
-  
   constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
-  onImagePicked(event: Event){
-    const file = (event.target as HTMLInputElement).files[0];
-    const reader = new FileReader();
-    reader.onload = ()=>{
-      this.imagePreview = reader.result.toString();
     };
-    reader.readAsDataURL(file);
-    console.log(this.imagePreview);
-  }
-  onSaveAvatar(){
-    // this.userService.updateAvatar();
-  }
 }
