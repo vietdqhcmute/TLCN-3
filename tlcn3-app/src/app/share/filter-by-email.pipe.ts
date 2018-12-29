cfa73849ec2c438dec4411c11d92f7c4e1f4e383
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-12-29 13:55:53 +0700

    upgrade search email engine

--- tlcn3-app/src/app/share/filter-by-email.pipe.ts
+++ tlcn3-app/src/app/share/filter-by-email.pipe.ts
@@ -1,16 +1,20 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: "emailFilter"
+  name: "emailFilter",
+  pure: false // this maybe bad behavior (trigger every change of data)
 })
 export class EmailFiterPipe implements PipeTransform {
   transform(value: any, filterString: string, propName: string): any {
-    if (value.length === 0 || filterString === ''){
-        return value;
+    if (value.length === 0 || filterString === "") {
+      return value;
     }
     const resultArray = [];
     for (const item of value) {
-      if (item[propName] === filterString) {
+      //   if (item[propName] === filterString) {
+      //     resultArray.push(item);
+      //   }
+      if (item[propName].includes(filterString) ) {
         resultArray.push(item);
       }
     }
