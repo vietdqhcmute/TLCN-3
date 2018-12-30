1fcd02f5b7d0ea48ff480f6bd2c7c55db7b86ed8
Quoc Viet Do <vietdqhcmute@gmail.com>
2018-12-31 01:17:28 +0700

    creating static chart ok!

--- tlcn3-app/src/app/page-admin/page-admin-dashboard/page-admin-dashboard.component.ts
+++ tlcn3-app/src/app/page-admin/page-admin-dashboard/page-admin-dashboard.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Chart } from "chart.js";
 @Component({
-  selector: 'app-page-admin-dashboard',
-  templateUrl: './page-admin-dashboard.component.html',
-  styleUrls: ['./page-admin-dashboard.component.scss']
+  selector: "app-page-admin-dashboard",
+  templateUrl: "./page-admin-dashboard.component.html",
+  styleUrls: ["./page-admin-dashboard.component.scss"]
 })
 export class PageAdminDashboardComponent implements OnInit {
+  @ViewChild('canvas') canvas: ElementRef;
 
-  constructor() { }
+  title = "Dash board";
+  chart:any=[];
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
+    var ctx = document.getElementById("myChart");
+    this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: "#3cba9f",
+            fill: false
+          },
+          {
+            data: [3, 2, 5, 3, 19, 12],
+            borderColor: "#ffcc00",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [
+            {
+              display: true
+            }
+          ],
+          yAxes: [
+            {
+              display: true
+            }
+          ]
+        }
+      }
+    });
+    console.log("this chart is" +this.chart);
   }
-
 }
