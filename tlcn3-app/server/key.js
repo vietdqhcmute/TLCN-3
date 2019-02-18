eb516645b64da87ed338c99a23a7a902ae40003a
Quoc Viet Do <32156408+vietdqhcmute@users.noreply.github.com>
2019-02-18 22:16:11 +0700

    Update key.js

--- tlcn3-app/server/key.js
+++ tlcn3-app/server/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    AWS_SECRET_KEY: 'ZSfVgX5r7hOn8F3XFO0ypiAkJu60ht/VIdQuf+cp',
-    AWS_ACCESS_KEY: 'AKIAIAM5U5LF5DGKJ42Q'
+    AWS_SECRET_KEY: '',
+    AWS_ACCESS_KEY: ''
   };
