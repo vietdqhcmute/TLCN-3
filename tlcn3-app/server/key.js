03f0caae2aa8697118f29ac637166f8b96d463c0
Quoc Viet Do <32156408+vietdqhcmute@users.noreply.github.com>
2019-02-27 23:23:19 +0700

    Delete key.js

--- tlcn3-app/server/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // AWS_SECRET_KEY: '2E+2MQOra2XpCtvVlxaf3uujzFh0FkAWLwRokgFc',
-    // AWS_ACCESS_KEY: 'AKIAJ457A73567URTLHQ'
-    AWS_SECRET_KEY: '',
-    AWS_ACCESS_KEY: ''
-  };
